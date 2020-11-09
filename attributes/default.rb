ff838708fddca695d35aaa3f446849a0c3e20c72
Jimmy Provencher <jimmy.provencher@cgi.com>
2020-11-09 04:09:17 -0500

    Upgrade to a dynamical way

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,11 @@
 
 # Array of repository names. Do not include ".git" extensions.
 default['git-server']['repositories'] = []
+default['git-server']['user'] = "git"
+default['git-server']['group'] = "git"
+default['git-server']['home'] = "/home/git"
+default['git-server']['shell'] = "/usr/bin/git-shell"
+default['git-server']['user_comment'] = "ssh_keys"
+default['git-server']['user_data_bag'] = "user"
+default['git-server']['username_data_bag'] = "username"
+default['git-server']['ssh_keys_data_bag'] = "ssh_keys"
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,36 +4,34 @@
 #
 #
 
-include_recipe 'git'
-
 # Create git user on server
-user "git" do
+user node['git-server']['user'] do
   supports :manage_home => true
-  comment "Git user"
-  home "/home/git"
-  shell "/usr/bin/git-shell"
+  comment node['git-server']['user_comment']
+  home node['git-server']['home']
+  shell node['git-server']['shell']
 end
 
-directory "/home/git/.ssh" do
-  user "git"
-  group "git"
+directory File.join(node['git-server']['home'], ".ssh") do
+  user node['git-server']['user']
+  group node['git-server']['group']
   mode "700"
 end
 
 # Pulls all SSH Keys out of users databag and adds to the git user
 # authorized_keys.  See users cookbook for details"
-users = data_bag('users')
+users = data_bag(node['git-server']['user_data_bag'])
 ssh_keys = ''
 users.each do |username|
-  user = data_bag_item('users', username)
-  Array(user['ssh_keys']).each do |ssh_key|
+  user = data_bag_item(node['git-server']['user_data_bag'], node['git-server']['username_data_bag'])
+  Array(user[node['git-server']['ssh_keys_data_bag']]).each do |ssh_key|
     ssh_keys << ssh_key + "\n"
   end
 end
 
-file "/home/git/.ssh/authorized_keys" do
-  owner "git"
-  group "git"
+file File.join(node['git-server']['home'], File.join('.ssh', 'authorized_keys')) do
+  owner node['git-server']['user']
+  group node['git-server']['group']
   mode "600"
   content ssh_keys
 end
@@ -41,9 +39,9 @@ end
 # Setup repositories defined as node attributes
 node['git-server']['repositories'].each do |repository_name|
   execute "git init --bare #{repository_name}.git" do
-    user "git"
-    group "git"
-    cwd "/home/git"
-    creates "/home/git/#{repository_name}.git"
+    user node['git-server']['user']
+    group node['git-server']['group']
+    cwd node['git-server']['home']
+    creates File.join(node['git-server']['home'], "#{repository_name}.git")
   end
 end
