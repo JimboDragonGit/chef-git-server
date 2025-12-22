208b85043c9bf83a71e61f2bf0091f6b1e9954fa
Jimmy Provencher <jimmy.provencher@hotmail.ca>
2025-12-22 01:56:36 -0500

    Bump version to 1.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'brian.hartsock@gmail.com'
 license          'All rights reserved'
 description      'Sets up a simple SSH based git server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.1'
+version          '1.1.0'
 chef_version '>= 16.6.14'
 
 depends 'ssh_authorized_keys'
